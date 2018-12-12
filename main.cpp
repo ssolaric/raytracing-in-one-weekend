4f4639a14e7c2d94d640a59bf2a2fb0ff85bd1c7
Sebastián Solari <35378033+ssolaric@users.noreply.github.com>
2018-12-12 18:19:21 -0500

    Hello World

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int nx = 200;
+    int ny = 100;
+    std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+    // Estos for recorren la imagen de tal forma que los ejes coordenados X e Y empiezan en la esquina
+    // inferior derecha y apuntan hacia la derecha y hacia arriba, respectivamente.
+    for (int j = ny - 1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            float r = float(i) / float(nx);
+            float g = float(j) / float(nx);
+            float b = 0.2;
+            int ir = int(255.99 * r);
+            int ig = int(255.99 * g);
+            int ib = int(255.99 * b);
+            std::cout << ir << " " << ig << " " << ib << "\n";
+        }
+    }
 }
