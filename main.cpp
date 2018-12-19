7e6f29cdb609b2a9dba72099080dfc9a302a59b4
Sebastián Solari <35378033+ssolaric@users.noreply.github.com>
2018-12-19 18:23:30 -0500

    Ahora soporta listas de hitables

--- main.cpp
+++ main.cpp
@@ -1,36 +1,25 @@
 #include <iostream>
 #include <cmath>
+#include <cfloat>
 #include "vec3.h"
 #include "ray.h"
-
-// Devuelve el primer punto en donde el rayo choca a la esdera, -1.0f si no la choca.
-float hit_sphere(const vec3& center, float radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    float a = dot(r.direction(), r.direction());
-    float b = 2.0f * dot(oc, r.direction());
-    float c = dot(oc, oc) - radius * radius;
-    float discriminant = b * b - 4 * a * c;
-    if (discriminant < 0) {
-        return -1.0f;
-    }
-    else {
-        return (-b - std::sqrt(discriminant)) / (2.0f * a);
-    }
-}
+#include "hitable.h"
+#include "sphere.h"
+#include "hitable_list.h"
 
 // Para visualizar las normales, se mapea cada componente  al intervalo [0, 1] (normalizar) y luego
 // se mapea (x, y, z) a (r, g, b).
-vec3 color(const ray& r) {
-    float t = hit_sphere(vec3(0, 0, -1), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0, 0, -1));
-        return 0.5 * vec3(N.x() + 1, N.y() + 1, N.z() + 1);
+vec3 color(const ray& r, hitable* world) {
+    hit_record rec;
+    if (world->hit(r, 0.0f, FLT_MAX, rec)) {
+        return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
     }
-
-    vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5f * (unit_direction.y() + 1.0f);
-    // Interpolación de colores
-    return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    else {
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5f * (unit_direction.y() + 1.0f);
+        // Interpolación de colores
+        return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+    }
 }
 
 int main() {
@@ -45,6 +34,11 @@ int main() {
     // El origen es la esquina inferior izquierda de la pantalla.
     vec3 origin(0.0, 0.0, 0.0);
 
+    hitable* list[2];
+    list[0] = new sphere(vec3(0, 0, -1), 0.5);
+    list[1] = new sphere(vec3(0, -100.5, -1), 100);
+    hitable* world = new hitable_list(list, 2);
+
     // Estos for recorren la imagen de tal forma que los ejes coordenados X e Y empiezan en la esquina
     // inferior derecha y apuntan hacia la derecha y hacia arriba, respectivamente.
     for (int j = ny - 1; j >= 0; j--) {
@@ -52,7 +46,7 @@ int main() {
             float u = float(i) / float(nx);
             float v = float(j) / float(ny);
             ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            vec3 col = color(r);
+            vec3 col = color(r, world);
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
             int ib = int(255.99 * col[2]);
