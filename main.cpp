9267f6f45c5ad6bfcae1ed248574cf107cfaf33d
Sebastián Solari <35378033+ssolaric@users.noreply.github.com>
2018-12-19 17:35:35 -0500

    Añadir función de color

--- main.cpp
+++ main.cpp
@@ -1,15 +1,34 @@
 #include <iostream>
 #include "vec3.h"
+#include "ray.h"
+
+vec3 color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5f * (unit_direction.y() + 1.0f);
+    // Interpolación de colores
+    return (1.0f - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
 
 int main() {
     int nx = 200;
     int ny = 100;
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+
+    vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
+    // Estos vectores no necesitan ser unitarios.
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    // El origen es la esquina inferior izquierda de la pantalla.
+    vec3 origin(0.0, 0.0, 0.0);
+
     // Estos for recorren la imagen de tal forma que los ejes coordenados X e Y empiezan en la esquina
     // inferior derecha y apuntan hacia la derecha y hacia arriba, respectivamente.
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+            float u = float(i) / float(nx);
+            float v = float(j) / float(ny);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+            vec3 col = color(r);
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
             int ib = int(255.99 * col[2]);
