cd70a150cf2c9c93afa4a4ba19c4579b9d07a759
Sebastián Solari <35378033+ssolaric@users.noreply.github.com>
2018-12-19 20:42:52 -0500

    Añadir fuzziness

--- metal.h
+++ metal.h
@@ -14,12 +14,14 @@ public:
 
     bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const override {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-        scattered = ray(rec.p, reflected);
+        // Añadir fuzziness para randomizar la dirección del rayo reflejado
+        scattered = ray(rec.p, reflected + fuzz * random_in_unit_sphere());
         attenuation = albedo;
         return dot(scattered.direction(), rec.normal) > 0;
     }
 
     vec3 albedo;
+    float fuzz = 0.3;
 };
 
 
