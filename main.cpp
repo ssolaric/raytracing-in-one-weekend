49a048531de6b43b4e13a23456f89e9975311f2b
Sebastián Solari <35378033+ssolaric@users.noreply.github.com>
2018-12-21 06:11:53 -0500

    Fin

--- main.cpp
+++ main.cpp
@@ -33,37 +33,47 @@ vec3 color(const ray& r, hitable* world, int depth) {
     }
 }
 
+hitable* random_scene() {
+    int n = 500;
+    hitable** list = new hitable*[n + 1];
+    list[0] = new sphere(vec3(0,-1000, 0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
+    int i = 1;
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            float choose_mat = drand48();
+            vec3 center(a + 0.9 * drand48(), 0.2, b + 0.9 * drand48());
+            if ((center - vec3(4, 0.2, 0)).length() > 0.9) {
+                if (choose_mat < 0.8) { // diffuse
+                    list[i++] = new sphere(center, 0.2, new lambertian(vec3(drand48() * drand48(), drand48() * drand48(), drand48() * drand48())));
+                }
+                else if (choose_mat < 0.95) { // metal
+                    list[i++] = new sphere(center, 0.2, new metal(vec3(0.5 * (1 + drand48()), 0.5 * (1 + drand48()), 0.5 * (1 + drand48())), 0.5 * drand48()));
+                }
+                else { // glass
+                    list[i++] = new sphere(center, 0.2, new dielectric(1.5));
+                }
+            }
+        }
+    }
+    list[i++] = new sphere(vec3(0, 1, 0), 1.0, new dielectric(1.5));
+    list[i++] = new sphere(vec3(-4, 1, 0), 1.0, new lambertian(vec3(0.4, 0.2, 0.1)));
+    list[i++] = new sphere(vec3(4, 1, 0), 1.0, new metal(vec3(0.4, 0.2, 0.1), 0.0));
+    return new hitable_list(list, i);
+}
+
 int main() {
-    int nx = 200;
-    int ny = 100;
-    int ns = 100;
+    int nx = 1200;
+    int ny = 800;
+    int ns = 10;
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
 
-    vec3 lower_left_corner(-2.0f, -1.0f, -1.0f);
-    // Estos vectores no necesitan ser unitarios.
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    // El origen es la esquina inferior izquierda de la pantalla.
-    vec3 origin(0.0, 0.0, 0.0);
+    hitable* world = random_scene();
 
-    hitable* list[5];
-//    float R = cos(M_PI / 4);
-    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.1, 0.2, 0.5)));
-    list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-    list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
-    list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
-//    list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
-//    list[3] = new sphere(vec3(-1, 0, -1), 0.5, new dielectric(1.5));
-    // Si se usa un radio negativo, las normales apuntan hacia adentro, es decir, se puede usar
-    // como una burbuja para mostrar una esfera hueca de vidrio.
-    list[4] = new sphere(vec3(-1, 0, -1), -0.45, new dielectric(1.5));
-    hitable* world = new hitable_list(list, 5);
-
-
-    vec3 lookfrom(3, 3, 2);
-    vec3 lookat(0, 0, -1);
-    float dist_to_focus = (lookfrom - lookat).length();
-    float aperture = 2.0;
+    vec3 lookfrom(13, 2, 3);
+    vec3 lookat(0, 0, 0);
+//    float dist_to_focus = (lookfrom - lookat).length();
+    float dist_to_focus = 10.0;
+    float aperture = 0.1;
     camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx) / float(ny), aperture, dist_to_focus);
     // Estos for recorren la imagen de tal forma que los ejes coordenados X e Y empiezan en la esquina
     // inferior derecha y apuntan hacia la derecha y hacia arriba, respectivamente.
--- metal.h
+++ metal.h
@@ -10,7 +10,14 @@
 
 class metal: public material {
 public:
-    explicit metal(const vec3& a): albedo(a) {}
+    explicit metal(const vec3& a, float f): albedo(a) {
+        if (f < 1) {
+            fuzz = f;
+        }
+        else {
+            fuzz = 1;
+        }
+    }
 
     bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const override {
         vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
@@ -21,7 +28,7 @@ public:
     }
 
     vec3 albedo;
-    float fuzz = 0.3;
+    float fuzz;
 };
 
 
